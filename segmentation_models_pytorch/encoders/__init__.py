cad507695e81a84fd2ed88c9cc3e9885618a8081
Starlento <33574105+Starlento@users.noreply.github.com>
2023-12-01 11:24:35 +0800

    hack encoder to provide self defined huggingface based encoders

--- segmentation_models_pytorch/encoders/__init__.py
+++ segmentation_models_pytorch/encoders/__init__.py
@@ -82,6 +82,8 @@ def get_encoder(name, in_channels=3, depth=5, weights=None, output_stride=32, **
                     list(encoders[name]["pretrained_settings"].keys()),
                 )
             )
+        if settings["url"] == "huggingface":
+            return encoder
         encoder.load_state_dict(model_zoo.load_url(settings["url"]))
 
     encoder.set_in_channels(in_channels, pretrained=weights is not None)
